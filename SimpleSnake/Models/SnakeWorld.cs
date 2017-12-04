55dc50e9769f0dc0fe09e160c26ffbbeff7883c3
Wingie Jagmoc <wingie.jagmoc@gmail.com>
2017-12-04 23:40:00 +0800

    Model file clean up

--- SimpleSnake/Models/SnakeWorld.cs
+++ SimpleSnake/Models/SnakeWorld.cs
@@ -6,33 +6,8 @@ using System.Threading.Tasks;
 
 namespace SimpleSnake.Models
 {
-    enum SnakeHeadDirection
-    {
-        Up = 1,
-        Right,
-        Down,
-        Left
-    }
-
-    enum MapContent
-    {
-        Space = 0,
-        SnakeBodyUp,
-        SnakeBodyQ1,
-        SnakeBodyRight
-    }
-
-
     class SnakeWorld
     {
-        private bool m_borderless;
-        private byte[,] m_mapContent;
-
-        private SnakeHeadDirection m_snakeHeadDirection;
-        private short m_snakeHeadX, m_snakeHeadY;
-        private short m_freshPreyX, m_freshPreyY;
-        private short m_eatenPreyX, m_eatenPreyY;
-
 
     }
 }
